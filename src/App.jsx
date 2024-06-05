38918b13bf550871d47b39144823d39650faabb3
tanayod <tanayod741@gmail.com>
2024-06-05 11:46:51 +0700

    feat:add state feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("Greeting Message");
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleOutput = () => {
+    setOutput(input);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{output}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={input}
+          onChange={handleInput}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleOutput}>Update text</button>
       </div>
     </div>
   );
